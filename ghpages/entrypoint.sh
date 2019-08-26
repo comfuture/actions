9017b249ee624224a95481d1421dcb271a1b4b91
comfuture <comfuture@gmail.com>
2019-08-27 02:01:44 +0900

    refactory ghpages action

--- ghpages/entrypoint.sh
+++ ghpages/entrypoint.sh
@@ -2,31 +2,31 @@
 
 set -e
 
-echo "#################################################"
-echo "Changing directory to 'BUILD_DIR' $BUILD_DIR ..."
+echo "cd $BUILD_DIR"
 cd $BUILD_DIR
 
-echo "#################################################"
-echo "Now deploying to GitHub Pages..."
-REMOTE_REPO="https://${GH_PAT}@github.com/${GITHUB_REPOSITORY}.git" && \
-REPONAME="$(echo $GITHUB_REPOSITORY| cut -d'/' -f 2)" && \
-OWNER="$(echo $GITHUB_REPOSITORY| cut -d'/' -f 1)" && \
-GHIO="${OWNER}.github.io" && \
-if [[ "$REPONAME" == "$GHIO" ]]; then
-  REMOTE_BRANCH="master"
+REPO="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+OWNER="$(echo $GITHUB_REPOSITORY | cut -d'/' -f 1)"
+REPONAME="$(echo $GITHUB_REPOSITORY | cut -d'/' -f 2)"
+PAGES_REPO="${OWNER}.github.io"
+if [[ "$REPONAME" == "$PAGES_REPO" ]]; then
+  TARGET_BRANCH="master"
 else
-  REMOTE_BRANCH="gh-pages"
-fi && \
-git init && \
-git config user.name "${GITHUB_ACTOR}" && \
-git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" && \
+  TARGET_BRANCH="gh-pages"
+fi
+
+: "${REMOTE_BRANCH:=TARGET_BRANCH}"
+
+git init
+git config user.name "${GITHUB_ACTOR}"
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 if [ -z "$(git status --porcelain)" ]; then
-    echo "Nothing to commit" && \
+    echo "Nothing to publish"
     exit 0
-fi && \
-git add . && \
-git commit -m 'Deploy to GitHub pages' && \
-git push --force $REMOTE_REPO master:$REMOTE_BRANCH && \
-rm -fr .git && \
-cd $GITHUB_WORKSPACE && \
+fi
+git add .
+git commit -m 'Deploy to GitHub pages'
+git push --force $REPO master:$REMOTE_BRANCH
+rm -rf .git
+cd $GITHUB_WORKSPACE
 echo "Content of $BUILD_DIR has been deployed to GitHub Pages."

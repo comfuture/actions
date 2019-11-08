b8b654b849d291d74a2e42e6f26eb8b3d997a39c
comfuture <comfuture@gmail.com>
2019-11-09 00:01:41 +0900

    change option

--- rsync/entrypoint.sh
+++ rsync/entrypoint.sh
@@ -2,11 +2,12 @@
 set -e
 
 # setup variables
-SSH_PORT=${INPUT_SSH_PORT:-22}
+PORT=${INPUT_PORT:-22}
 SOURCE=${INPUT_SOURCE:-.}
 HOST=$(echo $INPUT_DEST | cut -d ':' -f 1)
 
 echo "HOST=$HOST"
+echo "PORT=$PORT"
 echo "COMMAND=$INPUT_COMMAND"
 
 # copy private keys temporary
@@ -16,9 +17,9 @@ echo "$SSH_KEY" > $HOME/.ssh/deploy_key
 chmod 0400 "$HOME/.ssh/deploy_key"
 
 # run!
-sh -c "rsync $INPUT_OPTIONS -e 'ssh -i $HOME/.ssh/deploy_key -o StrictHostKeyChecking=no -p $SSH_PORT' $SOURCE $INPUT_DEST"
+sh -c "rsync $INPUT_OPTIONS -e 'ssh -i $HOME/.ssh/deploy_key -o StrictHostKeyChecking=no -p $PORT' $SOURCE $INPUT_DEST"
 
 # run after command!
 if [[ $INPUT_COMMAND ]]; then
-  ssh -i $HOME/.ssh/deploy_key -o StrictHostKeyChecking=no -A -tt -p $SSH_PORT $HOST "$INPUT_COMMAND"
+  ssh -i $HOME/.ssh/deploy_key -o StrictHostKeyChecking=no -A -tt -p $PORT $HOST "$INPUT_COMMAND"
 fi

115254142aa5c81dfc93e8ef247d9907fd2788b4
comfuture <comfuture@gmail.com>
2019-11-08 23:47:45 +0900

    use ssh-port instead of port

--- rsync/entrypoint.sh
+++ rsync/entrypoint.sh
@@ -6,6 +6,7 @@ SSH_PORT=${INPUT_SSH_PORT:-22}
 SOURCE=${INPUT_SOURCE:-.}
 HOST=$(echo $INPUT_DEST | cut -d ':' -f 1)
 
+
 # copy private keys temporary
 mkdir $HOME/.ssh
 chmod 0700 $HOME/.ssh
@@ -17,5 +18,5 @@ sh -c "rsync $INPUT_OPTIONS -e 'ssh -i $HOME/.ssh/deploy_key -o StrictHostKeyChe
 
 # run after command!
 if [[ $INPUT_COMMAND ]]; then
-  sh -c "ssh -i $HOME/.ssh/deploy_key -o StrictHostKeyChecking=no -p ${PORT:-22} $HOST '$INPUT_COMMAND'"
+  sh -c "ssh -i $HOME/.ssh/deploy_key -o StrictHostKeyChecking=no -p ${SSH_PORT:-22} $HOST '$INPUT_COMMAND'"
 fi

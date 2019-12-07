d80eac4c6e62e182c6f0d231f4a7d56caeb149c7
comfuture <comfuture@gmail.com>
2019-12-08 04:17:00 +0900

    ignore error when .ssh directory is already exists

--- ssh/entrypoint.sh
+++ ssh/entrypoint.sh
@@ -5,7 +5,7 @@ set -eu
 PORT=${INPUT_PORT:-22}
 
 # copy private keys temporary
-mkdir $HOME/.ssh
+mkdir -p $HOME/.ssh
 chmod 0700 $HOME/.ssh
 echo "$SSH_KEY" > $HOME/.ssh/deploy_key
 chmod 0400 "$HOME/.ssh/deploy_key"

3b4423213253c11ade5d4db7efaf21ebf0b5044e
comfuture <comfuture@gmail.com>
2019-12-08 04:58:02 +0900

    run command on full login shell including envs

--- ssh/entrypoint.sh
+++ ssh/entrypoint.sh
@@ -11,4 +11,4 @@ echo "$SSH_KEY" > $HOME/.ssh/deploy_key
 chmod 0400 "$HOME/.ssh/deploy_key"
 
 # run after command!
-ssh -i $HOME/.ssh/deploy_key -o StrictHostKeyChecking=no -A -tt -p ${PORT:-22} $INPUT_HOST "$INPUT_COMMAND"
+ssh -i $HOME/.ssh/deploy_key -o StrictHostKeyChecking=no -A -tt -p ${PORT:-22} $INPUT_HOST 'bash -l -c "$INPUT_COMMAND"'

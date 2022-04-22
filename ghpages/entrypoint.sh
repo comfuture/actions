7a28ea5e29a019de871b04a81f3b16dee92d3220
comfuture <comfuture@gmail.com>
2022-04-23 01:44:13 +0900

    git config explicit main branch name(2)

--- ghpages/entrypoint.sh
+++ ghpages/entrypoint.sh
@@ -21,6 +21,7 @@ git config --global init.defaultBranch main
 git config user.name "${GITHUB_ACTOR}"
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git init
+git branch -m main
 if [ -z "$(git status --porcelain)" ]; then
     echo "Nothing to publish"
     exit 0
@@ -30,6 +31,5 @@ git remote add origin "${REPO}"
 git add .
 git commit --allow-empty -m 'Deploy to GitHub pages'
 git push --force $REPO main:$REMOTE_BRANCH
-rm -rf .git
 cd $GITHUB_WORKSPACE
 echo "Content of $BUILD_DIR has been deployed to GitHub Pages."

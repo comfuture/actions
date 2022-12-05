a21cced5bbdee15147091037a18a914449e67cc8
comfuture <comfuture@iplateia.com>
2022-12-05 16:56:28 +0900

    delete oudated, unused actions

--- kubectl/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-# Extract the base64 encoded config data and write this to the KUBECONFIG
-echo "$KUBE_CONFIG_DATA" | base64 --decode > /tmp/kubeconfig
-export KUBECONFIG=/tmp/kubeconfig
-
-sh -c "kubectl $*"
